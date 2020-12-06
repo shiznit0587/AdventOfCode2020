c0c2d82653d1a5a36bc70c82093b27b65d971378
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2020-12-06 17:16:18 -0500

    Reorganized into folders per day, & added puzzles

--- src/Utils.fs
+++ src/Utils.fs
@@ -5,7 +5,7 @@ open System.IO
 open System.Text.RegularExpressions
 
 let readLines (day: int) =
-    File.ReadAllLines($"{__SOURCE_DIRECTORY__}/../inputs/Day{day}.txt")
+    File.ReadAllLines($"{__SOURCE_DIRECTORY__}/Day{day}/Input.txt")
 
 // Regular Expression active pattern: http://www.fssnip.net/29
 let (|Regex|_|) pattern input =
