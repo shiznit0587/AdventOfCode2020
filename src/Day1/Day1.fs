c9186bb3640759b7fe2e9e46ab4de84660b86401
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2020-12-09 18:24:49 -0500

    Optimized Day 1-B (10x improvement)

--- src/Day1/Day1.fs
+++ src/Day1/Day1.fs
@@ -4,10 +4,7 @@ let day1 =
     printfn "Running Day 1 - a"
 
     let lines =
-        readLines 1
-        |> Array.map int
-        |> Array.sort
-        |> Seq.toList
+        readLines 1 |> Array.map int |> Array.sort
 
     // Starting with pointers to the start and end of the sorted list,
     // recursively traverse our way inward until the sum of two numbers is 2020.
@@ -27,6 +24,8 @@ let day1 =
     // In functional programming, I can't figure out how to early-out when branches of a triple-nested loop sum > 2020
     // I'm going to instead just build all triples from the list and evaluate them all.
 
+    // All this list splitting is expensive (685ms). Let's try something else.
+    (*
     let rec makePairs list =
         match list with
         | [] -> []
@@ -44,10 +43,21 @@ let day1 =
                 pairs |> List.map (fun (y, z) -> (x, y, z))
 
             triples @ (makeTriples rest)
+    *)
+
+    // ~Now using for comprehension~ (was still slow-ish, at 196ms)
+    // Now using a yielding sequence (now 56ms)
+    let makeTriples (lines: array<'a>) =
+        seq {
+            for x in 0 .. lines.Length - 1 do
+                for y in x + 1 .. lines.Length - 1 do
+                    for z in y + 1 .. lines.Length - 1 do
+                        yield (lines.[x], lines.[y], lines.[z])
+        }
 
     let (x, y, z) =
         makeTriples lines
-        |> List.find (fun (x, y, z) -> x + y + z = 2020)
+        |> Seq.find (fun (x, y, z) -> x + y + z = 2020)
 
     printfn "x = %d, y = %d, z = %d, product = %d" x y z (x * y * z)
 
