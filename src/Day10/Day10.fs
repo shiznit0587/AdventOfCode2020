3c2407567fe4b62f04af2196c5d4c098ba2d1a1a
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2020-12-24 16:56:32 -0500

    Simplified Day 10 Solution

--- src/Day10/Day10.fs
+++ src/Day10/Day10.fs
@@ -4,23 +4,24 @@ let day10 =
     printfn "Running Day 10 - a"
 
     let input =
-        readLines 10 |> Array.map (int64) |> Array.sort
+        readLines 10
+        |> Array.map (int64)
+        |> Array.sort
+        |> Array.toList
+
+    let input =
+        [ 0L ] @ input @ [ List.last input + 3L ]
 
     let rec countDiffs idx ones threes =
-        if idx = 0 then
-            match input.[idx] with
-            | 1L -> countDiffs 1 1 0
-            | 3L -> countDiffs 1 0 1
-            | _ -> countDiffs 1 0 0
-        else if idx = input.Length then
-            (ones, threes + 1)
+        if idx = input.Length then
+            (ones, threes)
         else
             match input.[idx] - input.[idx - 1] with
             | 1L -> countDiffs (idx + 1) (ones + 1) threes
             | 3L -> countDiffs (idx + 1) ones (threes + 1)
             | _ -> countDiffs (idx + 1) ones threes
 
-    let (ones, threes) = countDiffs 0 0 0
+    let (ones, threes) = countDiffs 1 0 0
 
     printfn "1-Jolt Differences = %d, 3-Jolt Differences = %d, Product = %d" ones threes (ones * threes)
 
@@ -42,14 +43,6 @@ let day10 =
     for i in input.Length - 2 .. -1 .. 0 do
         pathCounts.[i] <- countPaths i
 
-    let getPathCounts i =
-        if input.[i] <= 3L then pathCounts.[i] else 0L
-
-    let finalPathCounts =
-        getPathCounts 0
-        + getPathCounts 1
-        + getPathCounts 2
-
-    printfn "Distinct Arrangements = %d" finalPathCounts
+    printfn "Distinct Arrangements = %d" pathCounts.[0]
 
     printfn "Day 10 Complete"
