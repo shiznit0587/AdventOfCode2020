2a0d8c29e3d32244a2f2426f3d8439b1895d06f3
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2020-12-24 00:08:16 -0500

    Day 10 Part 2 Attempt (WIP)

--- src/Day10/Day10.fs
+++ src/Day10/Day10.fs
@@ -26,4 +26,39 @@ let day10 =
 
     printfn "Running Day 10 - b"
 
+    // okay, I have a plan
+    // I think the number of permutations is strictly based on how many adapters can follow each adapter.
+    // This may not be true, because for each +1, it means the chains for a particular adapter are skipped.
+
+    // Will this work better working backwards?
+    // 19 -> c(22) -> 1
+    // 16 -> c(19) -> 1
+    // 15 -> c(16) -> 1
+    // 12 -> c(15) -> 1
+    // 11 -> c(12) -> 1
+    // 10 -> c(11) + c(12) -> 1 + 1 -> 2
+    //  7 -> c(10) -> 2
+    //  6 -> c( 7) -> 2
+    //  5 -> c( 6) + c(7) -> 2 + 2 -> 4
+    //  4 -> c( 5) + c(6) + c(7) -> 4 + 2 + 2 -> 8
+    //  1 -> c( 4) -> 8
+
+    let pathCounts = Array.zeroCreate input.Length
+    pathCounts.[pathCounts.Length - 1] <- 1
+
+    let getPathCounts idx offset =
+        if input.Length <= idx + offset then 0
+        else if input.[idx + offset] - input.[idx] > 3 then 0
+        else pathCounts.[idx + offset]
+
+    let countPaths i =
+        (getPathCounts i 1)
+        + (getPathCounts i 2)
+        + (getPathCounts i 3)
+
+    for i in input.Length - 2 .. -1 .. 0 do
+        pathCounts.[i] <- countPaths i
+
+    printfn "Distinct Arrangements = %d" pathCounts.[0]
+
     printfn "Day 10 Complete"
