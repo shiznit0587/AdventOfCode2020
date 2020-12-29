7c123254434b0ef4e75125201f8fa52185444b87
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2020-12-29 14:34:56 -0500

    Code Cleanup

--- src/Day1/Day1.fs
+++ src/Day1/Day1.fs
@@ -10,6 +10,7 @@ let day1 =
     // recursively traverse our way inward until the sum of two numbers is 2020.
     let rec findPairSumming2020 x y =
         let sum = lines.[x] + lines.[y]
+
         match sum with
         | sum when sum > 2020 -> findPairSumming2020 x (y - 1)
         | sum when sum < 2020 -> findPairSumming2020 (x + 1) y
@@ -24,29 +25,7 @@ let day1 =
     // In functional programming, I can't figure out how to early-out when branches of a triple-nested loop sum > 2020
     // I'm going to instead just build all triples from the list and evaluate them all.
 
-    // All this list splitting is expensive (685ms). Let's try something else.
-    (*
-    let rec makePairs list =
-        match list with
-        | [] -> []
-        | x :: rest -> // deconstruct the list into two parts - the first item and the rest of the list
-            let xPairs = rest |> List.map (fun y -> (x, y)) // make tuples
-            xPairs @ (makePairs rest) // append this list of pairs with the pairs from the rest of the list
-
-    let rec makeTriples list =
-        match list with
-        | [] -> []
-        | x :: rest ->
-            let pairs = makePairs rest
-
-            let triples =
-                pairs |> List.map (fun (y, z) -> (x, y, z))
-
-            triples @ (makeTriples rest)
-    *)
-
-    // ~Now using for comprehension~ (was still slow-ish, at 196ms)
-    // Now using a yielding sequence (now 56ms)
+    // Using for comprehension with a yielding sequence (now 56ms)
     let makeTriples (lines: array<'a>) =
         seq {
             for x in 0 .. lines.Length - 1 do
