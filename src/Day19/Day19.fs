18b7939b474344d59ec0c8a0ab7b8d1f43fbab4e
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2021-01-09 20:50:55 -0500

    Parsed Day 19 input

--- src/Day19/Day19.fs
+++ src/Day19/Day19.fs
@@ -1,7 +1,36 @@
 module Day19
 
+type RuleChoice =
+    | Text of string
+    | Rules of int array
+
+type Rule = { Id: int; Choices: RuleChoice array }
+
 let day19 =
     printfn "Running Day 19 - a"
+
+    let parseChoice s =
+        match s with
+        | Regex @"""(.)""" [ text ] -> Text(text)
+        | _ -> Rules(s.Split ' ' |> Array.map int)
+
+    let parseRule s =
+        match s with
+        | Regex @"(\d+): (.+)" [ id; choices ] ->
+            Some(
+                { Id = int id
+                  Choices = choices.Split " | " |> Array.map parseChoice }
+            )
+        | _ -> None
+
+    let input =
+        (readText 19)
+            .Split("\n\n", System.StringSplitOptions.TrimEntries)
+
+    let rules =
+        input.[0].Split '\n' |> Array.choose parseRule
+
+    let messages = input.[1].Split '\n'
 
     printfn "Running Day 19 - b"
 
