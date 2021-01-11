fa07844ae743778b5f0956b472735ad052def341
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2021-01-11 10:28:15 -0500

    Day 19 WIP

--- src/Day19/Day19.fs
+++ src/Day19/Day19.fs
@@ -1,26 +1,20 @@
 module Day19
 
 type RuleChoice =
-    | Text of string
+    | Text of char
     | Rules of int array
-
-type Rule = { Id: int; Choices: RuleChoice array }
 
 let day19 =
     printfn "Running Day 19 - a"
 
     let parseChoice s =
         match s with
-        | Regex @"""(.)""" [ text ] -> Text(text)
+        | Regex @"""(.)""" [ text ] -> Text(text.[0])
         | _ -> Rules(s.Split ' ' |> Array.map int)
 
     let parseRule s =
         match s with
-        | Regex @"(\d+): (.+)" [ id; choices ] ->
-            Some(
-                { Id = int id
-                  Choices = choices.Split " | " |> Array.map parseChoice }
-            )
+        | Regex @"(\d+): (.+)" [ id; choices ] -> Some(int id, choices.Split " | " |> Array.map parseChoice)
         | _ -> None
 
     let input =
@@ -28,9 +22,36 @@ let day19 =
             .Split("\n\n", System.StringSplitOptions.TrimEntries)
 
     let rules =
-        input.[0].Split '\n' |> Array.choose parseRule
+        input.[0].Split '\n'
+        |> Array.choose parseRule
+        |> Map
 
     let messages = input.[1].Split '\n'
+
+    let checkRules message rules =
+        // The rules list must match the message in its entirety.
+        // I have a list of rule IDs. Each one will have a list of rule choices.
+        // I don't know if I should care about the choices here. I think I have to.
+        // For each rule, for each choice, if that choice matches part of the message, continue with it, in case.
+        // This is 2D then, huh? ...
+        // Interestingly, I'm starting to wonder if this needs to return a list of matched message fragments
+        Some(message)
+
+    let checkRuleChoice (message:string) choice : Option<string> =
+        match choice with
+        | Text(c) -> if message.[0] = c then Some(message.[1..]) else None
+        | Rules(r) -> checkRule message r
+
+    // For each message, check if it matches rule 0.
+    let checkMessage message =
+        // I need to know how much of the string matches this rule.
+        let rec checkMessageRec message choices: Option<string> =
+
+
+        checkMessageRec message rules.[0]
+
+    let matchingCount =
+        messages |> Seq.choose checkMessage |> Seq.length
 
     printfn "Running Day 19 - b"
 
